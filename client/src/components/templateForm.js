d8e4391f7061d0ce0feaaa1c7264828dcb05d8c5
r-alo <email@example.com>
2022-08-13 13:03:10 -0500

    formControl initiated

--- client/src/components/templateForm.js
+++ client/src/components/templateForm.js
@@ -1,8 +1,31 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import FormLabel from "@mui/material/FormLabel";
+import FormControl from "@mui/material/FormControl";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormHelperText from "@mui/material/FormHelperText";
+import Checkbox from "@mui/material/Checkbox";
+
+
 
 export default function TemplateForm() {
+
+  const [state, setState] = React.useState({
+    html: false,
+    css: false,
+    javascript: false
+  });
+
+  const handleChange = (event) => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.checked
+    });
+  };
+
+  const { html, css, javascript } = state;
   return (
     <Box
       component="form"
@@ -51,6 +74,33 @@ export default function TemplateForm() {
         />
       </div>
 
+      <FormControl sx={ { m: 3 } } component="fieldset" variant="standard">
+        <FormLabel component="legend">Assign responsibility</FormLabel>
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Checkbox checked={ html } onChange={ handleChange } name="html" />
+            }
+            label="HTML"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox checked={ css } onChange={ handleChange } name="css" />
+            }
+            label="CSS"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={ javascript }
+                onChange={ handleChange }
+                name="javascript"
+              />
+            }
+            label="JavaScript"
+          />
+        </FormGroup>
+      </FormControl>
     </Box>
   );
 }
