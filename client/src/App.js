1a78ab1bc6f7a838b90f29534ac80029cd866998
HGuevaraCF <87338085+HGuevaraCF@users.noreply.github.com>
2022-08-13 12:21:25 -0500

    recruiter profile component

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,12 @@ import React from 'react';
 // import FreelanceProfile from './components/freelanceProfile.js';
 import FreelanceHighlight from './components/freelanceHighlight';
 import RecruiterProfile from './components/recruiterProfile';
+import FreelanceProfile from './components/freelanceProfile';
 
 function App() {
   return (
     <div className="App">
-      <RecruiterProfile/>
+      <FreelanceProfile/>
     </div>
   );
 }
