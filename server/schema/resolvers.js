3c668bb201a636f9d3035a4152da55d6bf072919
Daniel <daniel-alh@hotmail.com>
2022-08-16 17:55:41 -0500

    Add mutations to resolvers and tokens with JWT to create a user sesion, we can add profiles now, query for an specific user and validate user login

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -1,4 +1,6 @@
 const { Freelancer, Language, Framework, Platform, Knowledge } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
@@ -11,16 +13,42 @@ const resolvers = {
         platform: async () => {
             return await Platform.find({});
         },
-        freelancer: async () => {
+        freelancers: async () => {
             return await Freelancer.find({});
-        }
+        },
+        freelancer: async (parent, { profileId }) => {
+            return Freelancer.findOne({ _id: profileId });
+          },
     },
 
     Mutation: {
-        addLanguageTypeScript: async () => {
-            return await Language.create({language: 'TypeScript'})
-        }
+        // addLanguageTypeScript: async () => {
+        //     return await Language.create({language: 'TypeScript'})
+        // },
+
+        addProfile: async (parent, { firstName, lastName, phone, github, company, email, password, languages, frameworks, platforms }) => {
+            const profile = await Freelancer.create({ firstName, lastName, phone, github, company, email, password, languages, frameworks, platforms });
+            const token = signToken(profile);
+        
+            return profile ;
+        },
+        login: async (parent, { email, password }) => {
+            const profile = await Freelancer.findOne({ email });
+      
+            if (!profile) {
+              throw new AuthenticationError('No profile with this email found!');
+            }
+      
+            const correctPw = await profile.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect password!');
+            }
+      
+            const token = signToken(profile);
+            return { token, profile };
+          },
     }
-};
+}
 
 module.exports = resolvers;
