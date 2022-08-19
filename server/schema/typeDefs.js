1afe3f5f971bf4f313e7d3a40dbc2bdb15a9dcf4
Richard Guarnieri <richard.gm@outlook.com>
2022-08-19 00:53:08 -0400

    modified addRecruiter typeDefs

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -66,9 +66,22 @@ const typeDefs = gql`
 
     }
 
+    input LanguageInput {
+        language: String
+    }
+    input FrameworkInput {
+        framework: String
+    }
+    input PlatformInput {
+        platform: String
+    }
+    input KnowledgeInput {
+        knowledge: String
+    }
+
     type Mutation {
         addLanguageTypeScript: Language
-        addFreelancer( firstName: String!, lastName: String!, phone: String!, github: String!, company: String!, email: String!, password: String!, language: [String!], framework: [String!], platform: [String!]): Auth
+        addFreelancer( firstName: String!, lastName: String!, phone: String!, github: String!, company: String!, email: String!, password: String!, languages: [LanguageInput], frameworks: [FrameworkInput], platforms: [PlatformInput], knowledge: [KnowledgeInput]): Auth
         addRecruiter(firstName: String!, lastName: String!, phone: String!, company: String!, email: String!, password: String!): Auth
         loginFreelancer(email: String!, password: String!): Auth
         loginRecruiter(email: String!, password: String!): Auth
