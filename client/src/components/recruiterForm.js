19fbadaa58bb66a1b3b9c4d7f911251fad62b7d3
HGuevaraCF <87338085+HGuevaraCF@users.noreply.github.com>
2022-08-18 20:23:58 -0500

    recruiter form debug

--- client/src/components/recruiterForm.js
+++ client/src/components/recruiterForm.js
@@ -22,123 +22,112 @@ export default function RecruiterForm() {
           theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
       }}
     >
-    <Container component="main" maxWidth="xs">
-    <Grid>
-    <Box
-      component="form"
-      noValidate
-      autoComplete="off"
-      sx={{
-        marginTop: 8,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-      }}
-    >
-        <Typography component="h1" variant="h4">
-            Sign in
-          </Typography>
-    <Box component="form" noValidate
-    sx={{
-      '& .MuiTextField-root': { m: 1, width: '25ch' },
-    }}>
-      <div>
-        <TextField
-        margin="normal"
-        autoFocus
-        variant="outlined"
-        color="primary.dark"
-        focused
-          required
-          id=""
-          label="Name"
-          name="Recruiter"
-        />
-        <TextField
-        margin="normal"
-        autoFocus
-        variant="outlined"
-        color="success"
-        focused
-          required
-          id=""
-          label="Surname"
-          name="Recruiter"
-        />
-        <TextField
-        margin="normal"
-        fullWidth
-        autoFocus
-        variant="outlined"
-        color="success"
-        focused
-          required
-          id=""
-          label="Company Name"
-          name="Recruiter"
-        />
-        <TextField
-        margin="normal"
-        fullWidth
-        autoFocus
-        variant="outlined"
-        color="success"
-        focused
-          required
-          id=""
-          label="Phone Number"
-          name="Recruiter"
-        />
-        <TextField
-        margin="normal"
-        fullWidth
-        autoFocus
-        variant="outlined"
-        color="success"
-        focused
-          required
-          id=""
-          label="Email"
-          name="Recruiter"
-        />
-        <TextField
-        margin="normal"
-        fullWidth
-        autoFocus
-        variant="outlined"
-        color="success"
-        focused
-          id=""
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-        />
-        <TextField
-        margin="normal"
-        fullWidth
-        autoFocus
-        variant="outlined"
-        color="success"
-        focused
-          id=""
-          label="Confirm password"
-          type="password"
-          autoComplete="current-password"
-        />
-        <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="success"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-        </Button>
-      </div>
-    </Box>
-    </Box>
-    </Grid>
-    </Container>
+      <Container component="main" maxWidth="xs">
+        <Grid>
+          <Box
+            component="form"
+            noValidate
+            autoComplete="off"
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Box component="form" noValidate
+              sx={{
+                '& .MuiTextField-root': { m: 1, width: '25ch' },
+              }}>
+              <div>
+                <TextField
+                  margin="normal"
+                  autoFocus
+                  variant="outlined"
+                  focused
+                  required
+                  id="name"
+                  label="Name"
+                  name="Recruiter"
+                />
+                <TextField
+                  margin="normal"
+                  autoFocus
+                  variant="outlined"
+                  focused
+                  required
+                  id="surname"
+                  label="Surname"
+                  name="Recruiter"
+                />
+                <TextField
+                  margin="normal"
+                  fullWidth
+                  autoFocus
+                  variant="outlined"
+                  focused
+                  required
+                  id="company-name"
+                  label="Company Name"
+                  name="Recruiter"
+                />
+                <TextField
+                  margin="normal"
+                  fullWidth
+                  autoFocus
+                  variant="outlined"
+                  focused
+                  required
+                  id="phone"
+                  label="Phone Number"
+                  name="Recruiter"
+                />
+                <TextField
+                  margin="normal"
+                  fullWidth
+                  autoFocus
+                  variant="outlined"
+                  focused
+                  required
+                  id="email"
+                  label="Email"
+                  name="Recruiter"
+                />
+                <TextField
+                  margin="normal"
+                  fullWidth
+                  autoFocus
+                  variant="outlined"
+                  focused
+                  id="password"
+                  label="Password"
+                  type="password"
+                  autoComplete="current-password"
+                />
+                <TextField
+                  margin="normal"
+                  fullWidth
+                  autoFocus
+                  variant="outlined"
+                  focused
+                  id="password-confirm"
+                  label="Confirm password"
+                  type="password"
+                  autoComplete="current-password"
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="success"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Sign In
+                </Button>
+              </div>
+            </Box>
+          </Box>
+        </Grid>
+      </Container>
     </Paper>
   );
 }
