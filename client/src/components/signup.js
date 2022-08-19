ff63c6e17cfb2bd6df550b78b706609e2a2210e0
HGuevaraCF <87338085+HGuevaraCF@users.noreply.github.com>
2022-08-18 19:54:35 -0500

    sign up select form bug fix

--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -1,53 +1,20 @@
 import React, { useState } from 'react'
 import { Grid, Paper, TextField, Button, InputLabel, Select, MenuItem, FormControl } from '@mui/material'
-
-import { useMutation } from '@apollo/client';
-import { LOGIN } from '../utils/mutations';
-import Auth from '../utils/Auth';
-import { Container, Stack } from '@mui/system';
 import NavBar from './navBar';
 
 export default function Signup() {
-    const [age, setAge] = useState('');
-    const [formState, setFormState] = useState({ email: '', password: '' });
-    const [login, { data, loading, error }] = useMutation(LOGIN);
+    const [type, setType] = useState('');
 
-    // update state based on form input changes
     const handleChange = (event) => {
-        const { name, value } = event.target;
-        setFormState({
-            ...formState,
-            [name]: value,
-        });
-    };
-
-    // submit form
-    const handleFormSubmit = async (event) => {
-        event.preventDefault();
-        console.log(formState);
-        try {
-            const { data } = await login({
-                variables: { ...formState },
-            });
-
-            Auth.login(data.login.token);
-        } catch (e) {
-            console.error(e);
-        }
-
-        // clear form values
-        setFormState({
-            email: '',
-            password: '',
-        });
+        setType(event.target.value);
     };
 
     return (
         <Grid>
             <NavBar />
-            <br/>
-            <br/>
-            <br/>
+            <br />
+            <br />
+            <br />
             <Paper className='sign-up' elevation={10}>
                 <Grid align='center'>
                     <h2>Sign up</h2>
@@ -57,21 +24,14 @@ export default function Signup() {
                     <Select
                         labelId="demo-simple-select-standard-label"
                         id="demo-simple-select-standard"
-                        value={age}
+                        value={type}
                         onChange={handleChange}
-                        label="Age"
+                        label="type"
                     >
-                        <MenuItem value="">
-                            <em>None</em>
-                        </MenuItem>
-                        <MenuItem>Freelancer</MenuItem>
-                        <MenuItem>Recruiter</MenuItem>
+                        <MenuItem value={'freelance'}>Freelance</MenuItem>
+                        <MenuItem value={'recruiter'}>Recruiter</MenuItem>
                     </Select>
                 </FormControl>
-                <Container>
-                    {/* Render del form */}
-                </Container>
-                <Button type='submit' color='primary' variant="contained"  onClick={handleFormSubmit}>Sign up</Button>
                 <p>Already have an account? <a href='/login'>Log in</a></p>
             </Paper>
         </Grid>
