e87748191085c1a12a5e099564a44489447dce72
HGuevaraCF <87338085+HGuevaraCF@users.noreply.github.com>
2022-08-20 12:08:48 -0500

    app deployment

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const connection = require('./config/connection');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schema');
 const { authMiddleware } = require('./utils/auth');
+const mongoose = require('mongoose');
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -14,9 +16,28 @@ const server = new ApolloServer({
     context: authMiddleware,
 });
 
+mongoose
+.connect(process.env.MONGODB_CONNECTION_STRING,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("MongoDB has beeen connected"))
+    .catch((err) => console.log(err));
+
 // middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Accessing the path module
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 const main = async () => {
     await connection();
